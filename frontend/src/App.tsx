29de14d91297b2428df1b9ba129ff5f94d272379
Augustine Namare <agNamare@gmail.com>
2024-04-14 13:00:19 +0300

    Delete frontend gitignore and fix bugs

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useState } from 'react'
 import './App.css'
 import { Button } from './components/ui/button'
-
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-   <Button>Click ME</Button>
+  <Button>Click ME</Button>
   )
 }
-
 export default App
