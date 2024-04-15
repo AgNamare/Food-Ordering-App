8bde454d526531c2c0b3893ced9c4101a77e4e21
Augustine Namare <agNamare@gmail.com>
2024-04-15 14:43:29 +0300

    Add Auth0

---
+++ frontend/src/auth/Auth0ProviderWIthNavigate.tsx
@@ -0,0 +1,32 @@
+import { AppState, Auth0Provider, User } from "@auth0/auth0-react";
+import React from "react"
+
+type Props = {
+  children: React.ReactNode;
+}
+
+const Auth0ProviderWIthNavigate = ({children}: Props) => {
+  const domain = import.meta.env.VITE_AUTH0_DOMAIN;
+  const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
+  const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL;
+
+  if(!domain || !clientId || !redirectUri) {
+    throw new Error("Unable to initialise Auth")
+  }
+
+  const onRedirectCallback = (appState?: AppState, user?:User) => {
+    console.log("User:", user)
+  }
+  return (
+    <Auth0Provider 
+      domain={domain}
+      clientId={clientId}
+      authorizationParams={{redirect_uri:redirectUri}}
+      onRedirectCallback={onRedirectCallback}
+      >
+        {children}
+      </Auth0Provider>
+  )
+}
+
+export default Auth0ProviderWIthNavigate
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client'
 import './global.css'
 import {BrowserRouter as Router} from "react-router-dom";
 import AppRoutes from './AppRoutes';
+import Auth0ProviderWIthNavigate from './auth/Auth0ProviderWIthNavigate';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Router>
-      <AppRoutes/>
+      <Auth0ProviderWIthNavigate>
+        <AppRoutes/>
+      </Auth0ProviderWIthNavigate>
     </Router>
   </React.StrictMode>,
 )
