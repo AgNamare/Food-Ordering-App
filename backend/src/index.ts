626ab83bb0b2e515dd1b88026fe337fcaf67e88d
Augustine Namare <agNamare@gmail.com>
2024-04-15 15:47:07 +0300

    Complete Create User functionality

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,7 @@ import express, {Request, Response} from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import "dotenv/config";
+import myUserRoute from "./routes/MyUserRoutes"
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string)
   .then(()=> console.log("Connected to database!"))
@@ -11,9 +12,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get("/test", async(req:Request, res: Response) => {
-  res.json({message:"Hello"});
-});
+app.use("/api/my/user", myUserRoute);
 
 app.listen(7000, ()=> {
   console.log("Server started on port 5000")
