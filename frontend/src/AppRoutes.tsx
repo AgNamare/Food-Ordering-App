d38751cd7d134e70fe69e117bf9050e6f625a705
Augustine Namare <agNamare@gmail.com>
2024-04-22 17:28:03 +0300

    Finish User Authentication by Create and Validate Auth0 token

--- frontend/src/AppRoutes.tsx
+++ frontend/src/AppRoutes.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes, Navigate } from "react-router-dom"
 import Layout from "./layouts/layout";
 import Homepage from "./pages/Homepage";
+import AuthCallbackPage from "./pages/AuthCallbackPage";
 
 const AppRoutes = ()=>{
   return(
     <Routes>
       <Route path="/" element={<Layout><Homepage /></Layout>} />
       <Route path="/user-profile" element={<span>User profile page</span>} />
+      <Route path="/auth-callback" element={< AuthCallbackPage />} />
       <Route path="*" element={<Navigate to="/" />} />
     </Routes>
   );
--- frontend/src/auth/Auth0ProviderWIthNavigate.tsx
+++ frontend/src/auth/Auth0ProviderWIthNavigate.tsx
@@ -1,31 +1,33 @@
-import { useCreateMyUser } from "@/api/MyUserApi";
-import { AppState, Auth0Provider, User } from "@auth0/auth0-react";
+import { Auth0Provider } from "@auth0/auth0-react";
 import React from "react"
+import { useNavigate } from "react-router-dom";
 
 type Props = {
   children: React.ReactNode;
 }
 
 const Auth0ProviderWIthNavigate = ({children}: Props) => {
-  const { createUser } = useCreateMyUser();
+  const navigate = useNavigate();
   const domain = import.meta.env.VITE_AUTH0_DOMAIN;
   const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
   const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL;
+  const audience = import.meta.env.VITE_AUTH0_AUDIENCE;
 
-  if(!domain || !clientId || !redirectUri) {
+  if(!domain || !clientId || !redirectUri || !audience) {
     throw new Error("Unable to initialise Auth")
   }
 
-  const onRedirectCallback = (appState?: AppState, user?:User) => {
-    if (user?.sub && user?.email) {
-      createUser({ auth0Id: user.sub, email: user.email})
-    }
+  const onRedirectCallback = () => {
+   navigate("/auth-callback")
   }
   return (
     <Auth0Provider 
       domain={domain}
       clientId={clientId}
-      authorizationParams={{redirect_uri:redirectUri}}
+      authorizationParams={{
+        redirect_uri: redirectUri,
+        audience
+      }}
       onRedirectCallback={onRedirectCallback}
       >
         {children}
