15fe963c6d268e7fe11d718cb3afa1e887e77e71
Augustine Namare <agNamare@gmail.com>
2024-04-22 21:09:40 +0300

    Finish creating update-user request

--- frontend/src/api/MyUserApi.tsx
+++ frontend/src/api/MyUserApi.tsx
@@ -36,3 +36,36 @@ export const useCreateMyUser = () => {
     isSuccess
   }
 }
+
+type updateMyUserRequest = {
+  name: string,
+  addressLine1: string,
+  city: string,
+  country: string
+}
+
+export const useUpdateMyUser = () => {
+  const { getAccessTokenSilently} = useAuth0();
+
+  const updateMyUserRequest = async (formData: updateMyUserRequest) => {
+    const accessToken = await getAccessTokenSilently();
+    const response = await fetch(`${API_BASE_URL}/api/my/user`, {
+      method: "PUT",
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(formData)
+    });
+
+    if (!response.ok) {
+      throw new Error("Failed to update user");
+    }
+
+    return response.json()
+  }
+
+  const {mutateAsync: updateUser, isLoading, isError, error, reset} = useMutation(updateMyUserRequest)
+
+  return {updateUser, isLoading}
+}
