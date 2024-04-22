cf86dfe0c51fcf6fce78ca9855da4fe3615c355b
Augustine Namare <agNamare@gmail.com>
2024-04-22 18:30:59 +0300

    Complete update user profile backend logic

--- backend/src/middleware/auth.ts
+++ backend/src/middleware/auth.ts
@@ -1,7 +1,53 @@
+import { Response, Request, NextFunction } from "express";
 import { auth } from "express-oauth2-jwt-bearer";
+import jwt from "jsonwebtoken";
+import User from "../models/user";
+
+declare global {
+  namespace Express {
+    interface Request {
+      userId: string,
+      auth0Id: string
+    }
+  }
+}
 
 export const jwtCheck = auth({
+  //authorization method to check if a valid auth token exists
   audience: process.env.AUTH0_AUDIENCE,
   issuerBaseURL:  process.env.AUTH0_ISSUER_BASE_URL,
   tokenSigningAlg: 'RS256'
 });
+
+
+// Middleware to get auth0Id from token in Authorization Header
+// and using the token get the mongoose Id
+export const jwtParse = async(req: Request, res: Response, next:NextFunction) => {
+  const {authorization} = req.headers;
+
+  if(!authorization || !authorization.startsWith("Bearer ")) {
+    return res.sendStatus(401)
+  }
+
+  //get the token which is after the word Bearer
+  //Bearer sdjfisoihidfhie9hfpow09ur
+  const token = authorization.split(" ")[1];
+
+  try {
+    const decoded = jwt.decode(token) as jwt.JwtPayload;
+    const auth0Id = decoded.sub;
+
+    const user = await User.findOne({ auth0Id })
+
+    if(!user) {
+      return res.sendStatus(401);
+    }
+
+    req.auth0Id = auth0Id as string;
+    req.userId = user._id.toString();
+
+    next();
+  } catch (error) {
+    return res.sendStatus(401)
+  }
+}
---
+++ backend/src/middleware/validation.ts
@@ -0,0 +1,18 @@
+// A middleware to handle validation of the update user parameters
+import { NextFunction, Response, Request } from "express";
+import { body, validationResult } from "express-validator";
+
+const handleValidationErrors = async(req:Request, res:Response, next:NextFunction) => {
+const errors = validationResult(req);
+if(!errors.isEmpty()) {
+  return res.status(400).json({ errors: errors.array() });
+}
+}
+
+export const validateMyUserRequest = [
+  body("name").isString().notEmpty().withMessage("Name must be a string"),
+  body("addressLine1").isString().notEmpty().withMessage("AddressLine1 must be a string"),
+  body("city").isString().notEmpty().withMessage("City must be a string"),
+  body("country").isString().notEmpty().withMessage("Country must be a string"),
+  handleValidationErrors,
+]
