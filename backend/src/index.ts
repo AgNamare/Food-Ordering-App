472d5c08681a575d2585ac54276ce89798109a25
Augustine Namare <agNamare@gmail.com>
2024-04-23 17:02:00 +0300

    Create Restaurant backend functionality complete

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,11 +3,19 @@ import mongoose from "mongoose";
 import cors from "cors";
 import "dotenv/config";
 import myUserRoute from "./routes/MyUserRoutes";
+import myRestaurantRoute from "./routes/MyRestaurantRoutes";
+import {v2 as cloudinary} from "cloudinary";
 
 mongoose
   .connect(process.env.MONGODB_CONNECTION_STRING as string)
   .then(() => console.log("Connected to database!"))
   .catch((e) => console.error("Error connecting to database", e));
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret : process.env.CLOUDINARY_API_SECRET,
+})
 
 const app = express();
 app.use(express.json());
@@ -26,6 +34,7 @@ const corsOptions: cors.CorsOptions = {
 app.use(cors(corsOptions));
 
 app.use("/api/my/user", myUserRoute);
+app.use("/api/my/restaurant", myRestaurantRoute)
 
 app.listen(5000, () => {
   console.log("Server started on port 5000");
