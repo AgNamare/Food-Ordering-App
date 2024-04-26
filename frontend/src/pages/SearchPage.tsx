7298f1606e27804c5f175c5705cbdee5029db49f
Augustine Namare <agNamare@gmail.com>
2024-04-26 15:36:04 +0300

    Complete Search Page UI

---
+++ frontend/src/pages/SearchPage.tsx
@@ -0,0 +1,31 @@
+import { useSearchRestaurants } from "@/api/RestaurantApi";
+import SearchResultInfo from "@/components/SearchResultInfo";
+import SearchResultsCard from "@/components/SearchResultsCard";
+import { useParams } from "react-router-dom";
+
+const SearchPage = () => {
+  const { city } = useParams();
+  const { results, isLoading } = useSearchRestaurants(city);
+
+  if(isLoading) {
+    <span>Loading...</span>
+  }
+  if(!results?.data || !city) {
+    return <span>No results found</span>
+  }
+  return (
+    <div className="grid grid-cols-1 lg:grid-cols-[250px_1fr] gap-5">
+      <div id="cuisines-list">
+        insert cuisines here :)
+      </div>
+      <div id="main-content" className="flex flex-col gap-5">
+        <SearchResultInfo total={results.pagination.total} city={city} />
+        {results.data.map((restaurant)=>(
+          <SearchResultsCard restaurant={restaurant}/>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default SearchPage;
