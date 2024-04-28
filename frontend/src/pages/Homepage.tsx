0610e0b4552eb238f588fc10f4ea6cd6d88f3aa6
Augustine Namare <agNamare@gmail.com>
2024-04-28 13:58:43 +0300

    Put on searchQuery

--- frontend/src/pages/Homepage.tsx
+++ frontend/src/pages/Homepage.tsx
@@ -1,5 +1,5 @@
 import landingImage from "../assets/landing.png";
-import appDownloadImage from "../assets/appDownload.png"
+import appDownloadImage from "../assets/appDownload.png";
 import SearchBar, { SearchForm } from "@/components/SearchBar";
 import { useNavigate } from "react-router-dom";
 
@@ -7,9 +7,9 @@ const Homepage = () => {
   const navigate = useNavigate();
   const handleSearchSubmit = (searchFormValues: SearchForm) => {
     navigate({
-      pathname:`/search/${searchFormValues.searchQuery}`
-    })
-  }
+      pathname: `/search/${searchFormValues.searchQuery}`,
+    });
+  };
   return (
     <div className="flex flex-col gap-12">
       <div className="md:px-32 bg-white rounded-lg shadow-md py-8 flex flex-col gap-5 text-center -mt-16">
@@ -17,7 +17,11 @@ const Homepage = () => {
           Tuck into a takeway today
         </h1>
         <span className="text-xl">Food is just a click away!</span>
-        <SearchBar placeholder="Search by City or Town" onSubmit={handleSearchSubmit}/>
+        <SearchBar
+          placeholder="Search by City or Town"
+          searchQuery=""
+          onSubmit={handleSearchSubmit}
+        />
       </div>
       <div className="grid md-grid-cols-2 gap-5">
         <img src={landingImage} />
@@ -26,13 +30,14 @@ const Homepage = () => {
             Order takeaway even faster!
           </span>
           <span>
-            Download the bytes App for faster ordering and personalised recommendations
+            Download the bytes App for faster ordering and personalised
+            recommendations
           </span>
           <img src={appDownloadImage} />
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Homepage
+export default Homepage;
