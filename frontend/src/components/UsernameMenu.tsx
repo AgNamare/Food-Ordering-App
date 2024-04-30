bab57184b343cf4778812b71e0325041945ff007
Augustine Namare <agNamare@gmail.com>
2024-04-30 13:12:14 +0300

    Configure  auth0 logout url

--- frontend/src/components/UsernameMenu.tsx
+++ frontend/src/components/UsernameMenu.tsx
@@ -1,12 +1,22 @@
-import { CircleUserRound } from "lucide-react"
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "./ui/dropdown-menu"
-import { useAuth0 } from "@auth0/auth0-react"
+import { CircleUserRound } from "lucide-react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+import { useAuth0 } from "@auth0/auth0-react";
 import { Link } from "react-router-dom";
 import { Separator } from "./ui/separator";
 import { Button } from "./ui/button";
 
 const UsernameMenu = () => {
-  const {user, logout} = useAuth0();
+  const { user, logout } = useAuth0();
+  const returnToUrl = "https://edege-estate.onrender";
+
+  const handleLogout = () => {
+    logout({ logoutParams: { returnTo: returnToUrl } });
+  };
   return (
     <DropdownMenu>
       <DropdownMenuTrigger className="flex items-center px-3 font-bold hover:text-orange-500 gap-2">
@@ -20,17 +30,25 @@ const UsernameMenu = () => {
           </Link>
         </DropdownMenuItem>
         <DropdownMenuItem>
-          <Link to="/manage-restaurant" className="font-bold hover:text-orange-500">
+          <Link
+            to="/manage-restaurant"
+            className="font-bold hover:text-orange-500"
+          >
             Manage Restaurant
           </Link>
         </DropdownMenuItem>
         <Separator />
         <DropdownMenuItem>
-          <Button onClick={()=>logout()} className="flex flex-1 font-bold bg-orange-500">Log Out</Button>
+          <Button
+            onClick={handleLogout}
+            className="flex flex-1 font-bold bg-orange-500"
+          >
+            Log Out
+          </Button>
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
-  )
-}
+  );
+};
 
-export default UsernameMenu
+export default UsernameMenu;
