63b29303df1a75ee951ed2f4824eae58ebe7e39e
Augustine Namare <agNamare@gmail.com>
2024-04-30 14:09:13 +0300

    Fix syntax error

--- frontend/src/components/MobileNav.tsx
+++ frontend/src/components/MobileNav.tsx
@@ -1,43 +1,56 @@
-import { CircleUserRound, Menu } from "lucide-react"
-import { Sheet, SheetContent, SheetDescription, SheetTitle, SheetTrigger } from "./ui/sheet"
-import { Button } from "./ui/button"
-import { Separator } from "./ui/separator"
-import { useAuth0 } from "@auth0/auth0-react"
-import MobileNavLinks from "./MobileNavLinks"
+import { CircleUserRound, Menu } from "lucide-react";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetTitle,
+  SheetTrigger,
+} from "./ui/sheet";
+import { Button } from "./ui/button";
+import { Separator } from "./ui/separator";
+import { useAuth0 } from "@auth0/auth0-react";
+import MobileNavLinks from "./MobileNavLinks";
 
 const MobileNav = () => {
-  const { isAuthenticated, loginWithRedirect, user} = useAuth0();
+  const { isAuthenticated, loginWithRedirect, user } = useAuth0();
   const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
-  const mpesaTest = async()=> {
-    await fetch(`${API_BASE_URL}/payment/mpesa`)
-  }
+  const mpesaTest = async () => {
+    await fetch(`${API_BASE_URL}/api/payment/mpesa`);
+  };
   return (
     <Sheet>
       <SheetTrigger>
-        <Menu className= "text-orange-500"/>
+        <Menu className="text-orange-500" />
       </SheetTrigger>
       <SheetContent className="space-y-3">
         <SheetTitle>
-          {isAuthenticated? <span className="flex items-center font-bold gap-2">
-            <CircleUserRound className="text-orange-500" />
-            {user?.email}
-          </span> : (
+          {isAuthenticated ? (
+            <span className="flex items-center font-bold gap-2">
+              <CircleUserRound className="text-orange-500" />
+              {user?.email}
+            </span>
+          ) : (
             <span>Welcome to Bytes</span>
           )}
         </SheetTitle>
         <Separator />
         <SheetDescription className="flex flex-col gap-4">
-          {isAuthenticated ? <MobileNavLinks /> : (
+          {isAuthenticated ? (
+            <MobileNavLinks />
+          ) : (
             <Button
-              onClick={()=> loginWithRedirect()} 
-              className="flex-1 font-bold bg-orange-500">Log In</Button>
+              onClick={() => loginWithRedirect()}
+              className="flex-1 font-bold bg-orange-500"
+            >
+              Log In
+            </Button>
           )}
-          <Button onClick={mpesaTest} >MPESA TEST</Button>
+          <Button onClick={mpesaTest}>MPESA TEST</Button>
         </SheetDescription>
       </SheetContent>
     </Sheet>
-  )
-}
+  );
+};
 
-export default MobileNav
+export default MobileNav;
